b52443aa98d05060e564c56edf302f1252f047ff
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-03-10 09:57:13 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+// Global scope
+var customerName = 'bob';
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'someone';
+  leastFavoriteCustomer = 'new person'; // This will throw an error
+}
